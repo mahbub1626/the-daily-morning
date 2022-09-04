4c407d8e5daed9d4aec3dd33f30b802b0f261523
mahbub1626 <mahbub1626@gmail.com>
2022-09-04 22:24:49 +0600

    thumbnail img added

--- js/news.js
+++ js/news.js
@@ -5,24 +5,62 @@ const loadCategory = async () => {
     displaCategory(data.data.news_category)
 }
 
-// const displayphones = phones => {
-//     phones.forEach(phone => {
-//         const phoneDiv = document.createElement('div');
-//         phoneDiv.classList.add('col');
-//         phoneDiv.innerHTML = 
-
-
 const displaCategory = categories => {
     const categoryUl = document.getElementById('category-ul');
-    // console.log(categories);
     categories.forEach(category => {
-        // console.log(category.category_name);
         const li = document.createElement('li');
-        // li.classList.add('categoriesD');
-        li.innerText = category.category_name;
+        // li.classList.add('');
+        li.innerHTML = `
+        <a href="#" id="click-category">${category.category_name}</a>
+        `
         categoryUl.appendChild(li)
+        // console.log(category.category_id)
     })
+
 }
 
+// display 
+const newsLoade = async () => {
+    const url = `https://openapi.programming-hero.com/api/news/category/01`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayNews(data.data)
+}
 
+const displayNews = newses => {
+    // console.log(newses);
+    const newsContainer = document.getElementById('news-container');
+    newses.forEach(news => {
+        console.log(news.category_id)
+        // console.log(news);
+        const newsDiv = document.createElement("div");
+        newsDiv.classList.add('card');
+        newsDiv.innerHTML = `
+        <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="${news.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">${news.title}</h5>
+                            <p class="card-text">${news.details.length > 6000 ? news.details.slice(0, 6000) + '...' : news.details}</p>
+                            <div class="card-text">
+                            
+                            <img src="${news.author.img}" class="rounded rounded-circle" alt="..." style="width:50px; height:50px;"> 
+                            
+                            <span>
+                            <i>${news.author.name}</i>
+                            <small class="text-muted">${news.author.published_date}</small>
+                            </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        `;
+        newsContainer.appendChild(newsDiv);
+    })
+}
+newsLoade()
 loadCategory()
+
+
